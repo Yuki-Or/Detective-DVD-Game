dd066f6465c43f70ec339e6c5d94f495a85e542f
Yuki Orita <kkrr135792468@gmail.com>
2025-09-12 15:19:54 +0900

    face spawn & speed up

--- ShellinDVDGame/Assets/Scripts/FaceMovementController.cs
+++ ShellinDVDGame/Assets/Scripts/FaceMovementController.cs
@@ -1,16 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class FaceMovementController : MonoBehaviour
 {
     private float speed = 100f;
+    private float speedIncrement = 50f;
+    private float interval = 10f;
+    private float max_speed = 400f;
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Vector2 dir = new Vector2(Random.Range(-1f,1f), Random.Range(-1f,1f)).normalized;
+        Vector2 dir = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        rb.freezeRotation = true;
         rb.velocity = dir * speed;
+
+        StartCoroutine(IncreaseSpeedOverTime());
+    }
+
+      private IEnumerator IncreaseSpeedOverTime()
+    {
+        while (speed < max_speed)
+        {
+            yield return new WaitForSeconds(interval);
+
+            Vector2 direction = rb.velocity.normalized;
+            speed += speedIncrement;
+            rb.velocity = direction * speed;
+
+            Debug.Log($"speed is now {speed}");
+        }
     }
 }
--- ShellinDVDGame/Assets/Scripts/FaceSpawner.cs
+++ ShellinDVDGame/Assets/Scripts/FaceSpawner.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class FaceSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject facePrefab;
+    public Transform spawnParent;
+    private Vector2 spawnPosition;
+    private float spawnDelay = 5f;
+    private float secondSpawnDelay = 8f;
+
+    private float minX, maxX, minY, maxY;
+
     void Start()
     {
-        
+        minX = 190;
+        maxX = 200;
+        minY = 90;
+        maxY = 100;
+        StartCoroutine(SpawnAfterDelay(spawnDelay));
+        StartCoroutine(SpawnAfterDelay(secondSpawnDelay));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SpawnAfterDelay(float delay)
     {
-        
+        yield return new WaitForSeconds(delay);
+
+        Vector2 spawnPosition = new Vector2(
+            Random.Range(minX, maxX),
+            Random.Range(minY, maxY)
+        );
+
+        GameObject newFace = Instantiate(facePrefab, spawnPosition, Quaternion.identity, spawnParent);
     }
 }
